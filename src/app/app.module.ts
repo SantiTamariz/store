97dc8f44a3432f77860b22b9e436dbfecec68638
Stmarco <S.tmarco@alumnos.upm.es>
2022-06-06 09:19:14 +0200

    Ejemplo enrutamiento Santiago

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
+import { SantiagoComponent } from './santiago/santiago.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SantiagoComponent
   ],
   imports: [
     BrowserModule,
