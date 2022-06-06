92666c6a043cbd6fec24bef795f8b500324e19e7
Stmarco <S.tmarco@alumnos.upm.es>
2022-06-06 08:17:16 +0200

    Setting environment and prueba component

---
+++ src/app/prueba/prueba.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-prueba',
+  templateUrl: './prueba.component.html',
+  styleUrls: ['./prueba.component.scss']
+})
+export class PruebaComponent implements OnInit {
+
+  nombre: String = "Santiago Tamariz";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
